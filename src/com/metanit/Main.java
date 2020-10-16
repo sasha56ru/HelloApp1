bb44e1dc7787d02d82936b3b1a3e1e9b24e58126
Alexander Mayorov <ale5009@mail.ru>
2020-10-16 21:17:32 +0500

    2

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,11 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	System.out.println("Hello Java!");
+        System.out.println("Line");
+        System.out.println("Line little");
+    }
+}
